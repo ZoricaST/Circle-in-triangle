f15f2b2bd89c03925bca92cbaa771a8a31dfb25f
ZoricaST <zoricagithub@gmail.com>
2017-03-02 13:08:29 +0100

    Add files via upload

---
+++ krug.php
@@ -0,0 +1,23 @@
+ <html>
+<body>
+
+<form action="racunaj.php" method="post">
+Koordinate tacke A(x1,x2)<br>
+x1: <input type="text" name="x1"><br>
+y1: <input type="text" name="y1"><br>
+Koordinate tacke B(x2,y2)<br>
+x2: <input type="text" name="x2"><br>
+y2: <input type="text" name="y2"><br>
+Koordinate tacke C(x3,y3)<br>
+x3: <input type="text" name="x3"><br>
+y3: <input type="text" name="y3"><br>
+Koordinate tacke S(p,q) centra kruga<br>
+p: <input type="text" name="p"><br>
+q: <input type="text" name="q"><br>
+Poluprecnik kruga<br>
+r: <input type="text" name="r"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
